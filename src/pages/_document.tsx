fd7cd8ba45ae0454102c1b73b3c92a8e35be7d77
Kim <kells@kakao.com>
2023-03-15 15:10:06 +0900

    no document title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <link rel='shortcut icon' href='/icon.svg' type='image/x-icon' />
-        <title>Safewater</title>
       </Head>
       <body>
         <Main />
